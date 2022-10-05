31f7a854cc24abbac23c91e11555896ef8f49775
Shubhang <76577886+shubhangpatel@users.noreply.github.com>
2022-10-06 02:04:02 +0530

    Update max_subarray_sum_from_index_0_and_size.cpp

--- array/max_subarray_sum_from_index_0_and_size.cpp
+++ array/max_subarray_sum_from_index_0_and_size.cpp
@@ -1,1 +1,21 @@
-
+void solve()
+{
+  int n;
+  cin>>n;
+  vector<int>v(n);
+  int res=INT_MIN,tempres=0;
+  for(int &a:v) cin>>a;
+  for(int a=0;a<n;a++)
+  {
+     tempres+=v[a];
+     res=max(res,tempres);
+  }
+  tempres=0;
+  res=INT_MIN;
+  for(int a=n-1;a>=0;a--)
+  {
+    tempres+=v[a];
+    res=max(res,tempres);
+  }
+  cout<<res;
+}
