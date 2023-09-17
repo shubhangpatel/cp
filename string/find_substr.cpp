74f689d16d859d719ccf0f7f63e5c06e74f6edb1
Shubhang <76577886+shubhangpatel@users.noreply.github.com>
2023-09-17 18:31:13 +0530

    Update find_substr.cpp

--- string/find_substr.cpp
+++ string/find_substr.cpp
@@ -1,1 +1,22 @@
+//find postion of a char in a string
+// and subtring 
+#include<bits/stdc++.h>
+using namespace std;
+void find(string t)
+{
+    int tt=t.find(',');
+    //substring from 0 to pos
+    string a=t.substr(0,tt);
+    //substring from tt+1 to end pos
+    string b=t.substr(tt+1);
+    // 1 and tt inclusive
+    string c=t.substr(1,tt);
+    cout<<a<<" "<<b<<" "<<c;
+}
+int main()
+{
+     string t;
+     cin>>t;
+     find(t);
+} 
 
